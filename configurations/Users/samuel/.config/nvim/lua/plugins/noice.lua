cba4cd5c806ac24e5ae040ec870570499227c5d9
Samuel Piirainen <samuel@emblica.fi>
2022-11-19 16:42:42 +0200

    reduce noice message output

--- configurations/Users/samuel/.config/nvim/lua/plugins/noice.lua
+++ configurations/Users/samuel/.config/nvim/lua/plugins/noice.lua
@@ -2,10 +2,39 @@ require("noice").setup({
 	cmdline = {
 		view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
 	},
+	lsp = {
+		progress = {
+			enabled = false,
+		},
+	},
 	routes = {
 		{
 			view = "notify",
 			filter = { event = "msg_showmode" },
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "written",
+			},
+			opts = { skip = true },
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "less",
+			},
+			opts = { skip = true },
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "more",
+			},
+			opts = { skip = true },
 		},
 	},
 })
